010233278e43c1a9612e15dacf690429eb6d838f
Baiqing Lyu <baiqing@bu.edu>
2022-04-21 21:34:49 -0400

    Render out frame contents

--- src/main/java/gui/MainFrame.java
+++ src/main/java/gui/MainFrame.java
@@ -3,8 +3,12 @@ package gui;
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
+    private JPanel mainPanel;
+
     public MainFrame() {
         setTitle("Hello World");
-        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setContentPane(mainPanel);
+        pack();
     }
 }

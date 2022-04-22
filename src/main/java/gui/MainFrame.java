f1b403288e1f6898e48fb3d7f98c84cb00ea40bd
Baiqing Lyu <baiqing@bu.edu>
2022-04-21 21:41:22 -0400

    Set app title

--- src/main/java/gui/MainFrame.java
+++ src/main/java/gui/MainFrame.java
@@ -6,9 +6,8 @@ public class MainFrame extends JFrame {
     private JPanel mainPanel;
 
     public MainFrame() {
-        setTitle("Hello World");
+        super("Grade Calculator");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setContentPane(mainPanel);
         pack();
     }
 }

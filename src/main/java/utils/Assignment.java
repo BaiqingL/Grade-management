b495249d22301f171e1a58a67b821d762cf2caeb
BaiqingL <baiqing@bu.edu>
2022-04-27 22:34:15 -0400

    More auxiliary support with assignments

--- src/main/java/utils/Assignment.java
+++ src/main/java/utils/Assignment.java
@@ -3,10 +3,14 @@ package utils;
 public class Assignment {
     private String name;
     private int weight;
+    private int maxGrade;
+    private int grade;
 
-    public Assignment(String name, int weight) {
+    public Assignment(String name, int weight, int maxGrade, int grade) {
         this.name = name;
         this.weight = weight;
+        this.maxGrade = maxGrade;
+        this.grade = grade;
     }
 
     public String getName() {
@@ -24,4 +28,29 @@ public class Assignment {
     public void setName(String name) {
         this.name = name;
     }
+
+    public int getMaxGrade() {
+        return maxGrade;
+    }
+
+    public void setMaxGrade(int maxGrade) {
+        this.maxGrade = maxGrade;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public double getPercentage() {
+        return (double) grade / maxGrade;
+    }
+
+    public String getGradeString() {
+        return grade + "/" + maxGrade;
+    }
+
 }

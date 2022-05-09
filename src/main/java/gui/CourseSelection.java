d80fd31774889f55eb1f341d951169408bffbe71
mjqPauli <mjqpauli@bu.edu>
2022-05-09 17:48:33 -0400

    changed course view to be a table instead of a button

--- src/main/java/gui/CourseSelection.java
+++ src/main/java/gui/CourseSelection.java
@@ -2,6 +2,7 @@ package gui;
 
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
+import org.bouncycastle.jcajce.provider.drbg.DRBG;
 import utils.ClassPathTuple;
 import utils.CSVReader;
 import utils.GradedClass;
@@ -9,6 +10,7 @@ import utils.GradedClass;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.plaf.FontUIResource;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.text.StyleContext;
 import java.awt.*;
 import java.util.ArrayList;
@@ -24,11 +26,13 @@ public class CourseSelection extends JPanel {
     private JButton deleteCourseButton;
     private JButton logoutButton;
     private JPanel courseActions;
-    private JPanel courseTiles;
     private JPanel spacer;
     private JLabel semester;
+    private JScrollPane courseContainer;
 
-    private final List<CourseTile> tiles;
+    private JTable courseTable;
+
+    private DefaultTableModel model;
     private boolean isLoggedIn;
 
     private List<GradedClass> courses;
@@ -40,7 +44,6 @@ public class CourseSelection extends JPanel {
     public CourseSelection() {
 
         this.isLoggedIn = true;
-        tiles = new ArrayList<>();
         courses = new ArrayList<>();
         $$$setupUI$$$();
         addCourseButton.addActionListener(e -> {
@@ -53,44 +56,23 @@ public class CourseSelection extends JPanel {
                 filePath = fileChooser.getSelectedFile().getAbsolutePath();
             }
             addCourse(filePath);
-            count++;
-            System.out.println(tiles.size() + " courses added");
-
-            courseTiles.removeAll();
-
-            for (CourseTile ct : tiles) {
-                courseTiles.add(ct.getTilePanel());
-            }
-
-            courseTiles.revalidate();
-            courseTiles.updateUI();
             firePropertyChange("GUIupdate", 0, 0);
         });
         deleteCourseButton.addActionListener(e -> {
-            System.out.println(tiles.size() + " courses left");
 
-            if (tiles.size() > 0) {
-                String selected = JOptionPane.showInputDialog(null, "Select a course to delete: \n" + getCoursesAndIndex(), "Delete Course", JOptionPane.QUESTION_MESSAGE);
-                // Make sure the input is a number
-                try {
-                    while (selected != null && !isNumeric(selected) || Integer.parseInt(Objects.requireNonNull(selected)) > tiles.size()) {
-                        JOptionPane.showMessageDialog(null, "Please enter a valid number", "Error", JOptionPane.ERROR_MESSAGE);
-                        selected = JOptionPane.showInputDialog(null, "Select a course to delete: \n" + getCoursesAndIndex(), "Delete Course", JOptionPane.QUESTION_MESSAGE);
-                    }
-                    deleteCourse(Integer.parseInt(selected) - 1);
-                } catch (Exception ignored) {
-
-                }
-            } else {
-                JOptionPane.showMessageDialog(null, "No courses to delete", "Error", JOptionPane.ERROR_MESSAGE);
-            }
-
-            courseTiles.revalidate();
-            courseTiles.updateUI();
+            deleteCourse(courseTable.getSelectedRow());
             firePropertyChange("GUIupdate", 0, 0);
 
         });
+
         logoutButton.addActionListener(e -> loggout());
+
+        courseTable.getSelectionModel().addListSelectionListener(e -> {
+            // prevent the following function being called twice
+            if (!e.getValueIsAdjusting() && courseTable.getSelectedRow() != -1) {
+                firePropertyChange("CourseSelected", null, courses.get(courseTable.getSelectedRow()));
+            }
+        });
 
     }
 
@@ -115,26 +97,20 @@ public class CourseSelection extends JPanel {
         try {
             GradedClass course = CSVReader.loadCSV(filePath);
             courses.add(course);
+            model.addRow(new String[]{course.getClassName(), String.valueOf(course.getAssignments().size()), String.valueOf(course.getStudents().size())});
             CLASS_PATH_TUPLE.add(new ClassPathTuple(course.getClassName(), filePath));
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Cannot read from" + filePath);
         }
-        CourseTile ct = new CourseTile(courses.get(courses.size() - 1).toString(), tiles.size());
-        ct.addPropertyChangeListener(evt -> {
-            System.out.println("notified course selection page");
-            if (evt.getPropertyName().equals("isSelected")) {
-                firePropertyChange("CourseSelected", null, courses.get((int) evt.getNewValue()));
-            }
-        });
 
-        tiles.add(ct);
     }
 
     private void deleteCourse(int idx) {
-        CourseTile ct = tiles.remove(idx);
-        courseTiles.remove(ct.getTilePanel());
+
+        model.removeRow(idx);
         courses.remove(idx);
+
     }
 
     private void loggout() {
@@ -142,10 +118,18 @@ public class CourseSelection extends JPanel {
         firePropertyChange("isLoggedIn", true, false);
     }
 
+    private String[] getHeaders() {
+        String[] headers = {"Name", "Assignment Number", "Students Enrolled"};
+        return headers;
+    }
+
     private void createUIComponents() {
         // TODO: place custom component creation code here
-        courseTiles = new JPanel(new GridLayout(0, 4));
-        courseTiles.setBorder(new EmptyBorder(0, 30, 0, 0));
+        courseContainer = new JScrollPane();
+        model = new DefaultTableModel();
+        model.setColumnIdentifiers(getHeaders());
+        courseTable = new JTable(model);
+
     }
 
     public void setSemesterLabel(String semesterLabel) {
@@ -163,7 +147,6 @@ public class CourseSelection extends JPanel {
         createUIComponents();
         coursePanel = new JPanel();
         coursePanel.setLayout(new GridLayoutManager(3, 1, new Insets(0, 0, 0, 0), -1, -1));
-        coursePanel.add(courseTiles, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, 1, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         spacer = new JPanel();
         spacer.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
         coursePanel.add(spacer, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
@@ -187,6 +170,8 @@ public class CourseSelection extends JPanel {
         logoutButton = new JButton();
         logoutButton.setText("Logout");
         courseActions.add(logoutButton, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        coursePanel.add(courseContainer, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        courseContainer.setViewportView(courseTable);
     }
 
     /**
